96393a5ee0dc1733315a92b1279db6cd3d0a7587
Stanislav Povolotsky <stas.github@povolotsky.info>
2022-08-21 07:10:49 +0300

    тесты

--- tests/curl-test/test.cmd
+++ tests/curl-test/test.cmd
@@ -1,5 +1,11 @@
+@echo off
+set input_file=%~1
+if "%input_file%" == "" (
+  set input_file=test.json
+)
+set output_file=%input_file%.output.json
 curl -X "POST" ^
   "http://localhost:8100/match_products" ^
   -H "accept: application/json" ^
   -H "Content-Type: application/json" ^
-  -d @test.json -o test.output.json
+  -d "@%input_file%" -o %output_file%
